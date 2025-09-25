71276f05021c60ba7de9314b962ea5fc8eb0b777
Arel Raba <arelraba11@gmail.com>
2025-09-25 12:30:39 +0300

    Add business routes

---
+++ routes/business.js
@@ -0,0 +1,39 @@
+import express from "express";
+import BusinessInfo from "../models/BusinessInfo.js";
+
+const router = express.Router();
+
+// Fetch the business info (returns null if not set)
+router.get("/", async (req, res) => {
+  try {
+    const info = await BusinessInfo.findOne();
+    if (!info) {
+      return res.json(null);
+    }
+    res.json(info);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+// Create or update the business info
+router.post("/", async (req, res) => {
+  try {
+    let info = await BusinessInfo.findOne();
+    if (info) {
+      // Update existing business info
+      info.set(req.body);
+      await info.save();
+      return res.json(info);
+    } else {
+      // Create new business info
+      const newBusiness = new BusinessInfo(req.body);
+      await newBusiness.save();
+      return res.json(newBusiness);
+    }
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+export default router;
--- server.js
+++ server.js
@@ -1,19 +1,24 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import businessRoutes from "./routes/business.js";
 
+// Load environment variables
 dotenv.config();
 const app = express();
 
+// Middleware to parse JSON bodies
 app.use(express.json());
 
+// Business info routes
+app.use("/api/business", businessRoutes);
 
+// Basic route to check if server is running
 app.get("/", (req, res) => {
   res.send("Server is running!");
 });
 
-
+// Connect to MongoDB and start the server
 mongoose.connect(process.env.MONGO_URI)
-  .then(() => app.listen(5000, () => console.log("Server running on port 5000")))
+  .then(() => app.listen(4000, () => console.log("Server running on port 4000")))
   .catch((err) => console.log(err));
-
